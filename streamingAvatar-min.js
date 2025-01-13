0d20e91bd64882b5bb76e34053299c6ac9049276
唐仲惠 <pigmore@gmail.com>
2025-01-13 16:12:11 +0800

    fix: some css detail

--- streamingAvatar-min.js
+++ streamingAvatar-min.js
@@ -213,22 +213,49 @@ a=mid:video
       #messageWrap > div.right{
         justify-content: flex-end;
       }
-
-      #app {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
-      }
-      #app>div {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      }
       #controls {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         min-width: 100%;
+      }
+      button,input {
+        border-radius: 8px;
+        border: 1px solid transparent;
+        padding: 0.6em 1.2em;
+        font-size: 1em;
+        font-weight: 500;
+        font-family: inherit;
+        background-color: #1a1a1a;
+        cursor: pointer;
+        transition: border-color 0.25s;
+      }
+      button:hover,
+      input:hover {
+        border-color: #646cff;
+      }
+      button:disabled:hover,input:disabled:hover {
+        border-color: #646cff00;
+        cursor: not-allowed;
+      }
+
+      button:focus,
+      input:focus,
+      button:focus-visible
+      input:focus-visible {
+        outline: 4px auto -webkit-focus-ring-color;
+      }
+
+      @media (prefers-color-scheme: light) {
+        :root {
+          color: #213547;
+          background-color: #ffffff;
+        }
+        a:hover {
+          color: #747bff;
+        }
+        button {
+          background-color: #f9f9f9;
+        }
       }
     `,Cr=document.createElement("style");Cr.textContent=Ao,document.head.appendChild(Cr)}get classStatus(){return this.props()}get micStatus(){return this.micEnable}props(){return Object.fromEntries(Object.entries(this))}};window.StreamingAvatar = a5;
